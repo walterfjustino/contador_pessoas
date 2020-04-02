003f6f266b624667b7584cca0ba5edc198dfa544
Walter Fernandes Justino <walterfjustino@gmail.com>
2020-04-02 13:44:18 -0300

    App contador de pessoas

---
+++ contador_de_pessoas/android/app/src/main/kotlin/wfjprojects/contadordepessoas/com/br/MainActivity.kt
@@ -0,0 +1,12 @@
+package wfjprojects.contadordepessoas.com.br
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
